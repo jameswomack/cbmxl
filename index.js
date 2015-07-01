0e58e2b0a4451f65676f2435cc3f82a05eaaf359
James Womack <jwomack@netflix.com>
2015-07-01 12:28:08 -0700

    A Node HTTP server for showing a CBXML hierarchy

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-var FS = require('fs')
-var Assert = require('assert-plus')
-var XML = require('pixl-xml')
-var Entry = require('./lib/entry')
+var HTTP = require('http')
 var Console = require('./lib/console')
+var Reader = require('./lib/reader')
+var CBXMLList = require('./lib/cbxml-list-stream')
 
-var doc = XML.parse(FS.readFileSync('./dat/Snippets.cbxml'))
-Assert.object(doc)
+HTTP.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'})
+  Reader().readFromPath('./dat/Snippets.cbxml')
+    .on('complete', (xmlDocStream) => {
+      xmlDocStream
+        .pipe(CBXMLList())
+        .pipe(res)
+    })
+    .on('error', res.end)
+}).listen(1337, '127.0.0.1')
+Console.log('Server running at http://127.0.0.1:1337/')
 
-var _entries = doc.object
-Assert.arrayOfObject(_entries)
-
-var entries = _entries.map((_entry) => {
-  return Entry(_entry)
-})
-
-Console.dir(entries.filter((e) => {
-  return e.type === 'ASSET'
-}).map((e) => {
-  return e.expand().text
-}))
